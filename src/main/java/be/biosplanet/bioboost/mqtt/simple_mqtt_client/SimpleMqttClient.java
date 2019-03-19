bb2cce5804607cd352da4f221ec727a2f6fef243
Nico De Witte <nico.dewitte@vives.be>
2019-03-19 11:14:19 +0100

    Fix name of MQTT Client class

--- src/main/java/be/biosplanet/bioboost/mqtt/simple_mqtt_client/SimpleMqttClient.java
+++ src/main/java/be/biosplanet/bioboost/mqtt/simple_mqtt_client/SimpleMqttClient.java
@@ -4,8 +4,8 @@ package be.biosplanet.bioboost.mqtt.simple_mqtt_client;
  * The main class.
  *
  */
-public class SimpleMqttClient {
+public class SimpleMQTTClient {
 
-  public SimpleMqttClient() {
+  public SimpleMQTTClient() {
   }
 }
