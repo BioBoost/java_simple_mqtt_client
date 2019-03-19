7291a1033b71b700d63cc2e61882b7505db0766c
Nico De Witte <nico.dewitte@vives.be>
2019-03-19 11:22:00 +0100

    Add implementation of mqtt client

--- src/main/java/be/biosplanet/bioboost/mqtt/simple_mqtt_client/IMQTTMessageHandler.java
+++ src/main/java/be/biosplanet/bioboost/mqtt/simple_mqtt_client/IMQTTMessageHandler.java
@@ -5,5 +5,5 @@ package be.biosplanet.bioboost.mqtt.simple_mqtt_client;
  * @author BioBoost
  */
 public interface IMQTTMessageHandler {
-  public void messageArrived(String message, String topic);
+  public void messageArrived(String topic, String message);
 }
