8e79a2298d583ed154f9878bcb9a2f3f2c6c9b6a
Nico De Witte <nico.dewitte@vives.be>
2019-03-19 11:12:20 +0100

    Start of simple mqtt client library

---
+++ README.md
@@ -0,0 +1,1 @@
+# Simple MQTT Library in Java
---
+++ src/main/java/be/biosplanet/bioboost/mqtt/simple_mqtt_client/SimpleMqttClient.java
@@ -0,0 +1,11 @@
+package be.biosplanet.bioboost.mqtt.simple_mqtt_client;
+
+/**
+ * The main class.
+ *
+ */
+public class SimpleMqttClient {
+
+  public SimpleMqttClient() {
+  }
+}
